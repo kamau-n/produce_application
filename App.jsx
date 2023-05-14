bcf0342545b639f483bd65b3bf9afbe5a6e511c9
kamau-n <bsclmr146620@spu.ac.ke>
2023-05-14 18:27:41 +0300

    first commit

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <View className="">
+        <Text>Login Form</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
